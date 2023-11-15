9e6bf64819507032953167e0d13521cd8e02f8dc
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-11-14 23:01:51 -0300

    configurando o volume

--- src/script/engine.js
+++ src/script/engine.js
@@ -1,4 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+
+let mapedKeys = [];
 
 let audio = new Audio("src/tunes/a.wav");
 
@@ -16,8 +19,17 @@ const playTune = (key) => {
 
 pianoKeys.forEach((key) => {
     key.addEventListener("click",() => playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
-    playTune(e.key)
+    if (mapedKeys.includes(e.key)) {
+    playTune(e.key);
+    }
 });
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+};
+
+volumeSlider.addEventListener("input", handleVolume);
